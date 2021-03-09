b5c431e04e5dec6ccff35602c307d9590e482db0
qiaoyanlin <2667254268@qq.com>
2021-03-09 14:33:24 +0800

    添加test工程1

---
+++ src/hello.java
@@ -0,0 +1,15 @@
+/**
+ * Created by IntelliJ IDEA.
+ * ClassName: hello <br/>
+ * Description: <br/>
+ * User: QYL
+ * Date: 2021/3/9   14:28
+ */
+public class hello {
+    public static void main(String[] args) {
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+    }
+}
